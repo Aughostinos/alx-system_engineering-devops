a76312217d672ef038b47408685132e4c544d3a6
aughostinos <augustinos.nabil@gmail.com>
2024-03-22 16:39:15 -0700

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # This Puppet manifest installs Flak
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require => Package['python3-pip'],
 }
