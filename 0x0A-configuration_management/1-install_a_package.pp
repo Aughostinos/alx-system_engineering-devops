06c31c51164533d829ef25de7c1bfd7834a76fce
aughostinos <augustinos.nabil@gmail.com>
2024-03-22 16:58:48 -0700

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# This Puppet manifest installs Flak
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# This Puppet manifest installs Flak
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install-flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 freeze | grep Flask==2.1.0',
+  require => Package['python3-pip'],
 }
