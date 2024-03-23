97401c299b4ff53ecc18fcf8468b2b295bb456eb
aughostinos <augustinos.nabil@gmail.com>
2024-03-22 17:04:58 -0700

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,8 +4,12 @@ package { 'python3-pip':
   ensure => installed,
 }
 
-exec { 'install-flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  unless  => '/usr/bin/pip3 freeze | grep Flask==2.1.0',
-  require => Package['python3-pip'],
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
