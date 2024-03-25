58d74e3e120939f5ad0486f0fb2c4c5a79247907
aughostinos <augustinos.nabil@gmail.com>
2024-03-24 18:07:07 -0700

    fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,13 +2,8 @@
 #SSH client configuration must be configured to use the private key ~/.ssh/school
 #SSH client configuration must be configured to refuse to authenticate using a password
 
-file_line { 'identity file':
- ensure => 'present,
- path   => '/etc/ssh/ssh_config',
- line   => '	IdentityFile ~/.ssh/school',
-}
-
-file_line {' disabled password',
- path => '/etc/ssh/ssh_config',
- line => '	PasswordAuthentication no',
+file { '/etc/ssh/ssh_config':
+ content => " host 54.167.150.12
+              IdentityFile ~/.ssh/school
+              PasswordAuthentication no"
 }
