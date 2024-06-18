2f8481a78dc2398f0d56a62b8413b5617ea9ce1b
Aughostinos <augustinos.nabil@gmail.com>
2024-06-19 03:00:44 +0400

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,18 +3,16 @@
 import requests
 
 
-# define the base url
-api_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-headers = {
-    "User-Agent": "User 01"
-}
-
-
 def number_of_subscribers(subreddit):
     """a function that queries the Reddit API and returns the number
     of subscribers (not active users, total subscribers) for a given
     subreddit. If an invalid subreddit is given, the function should return 0.
     """
+    api_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+    "User-Agent": "User 01"
+    }
+
     response = requests.get(api_url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
