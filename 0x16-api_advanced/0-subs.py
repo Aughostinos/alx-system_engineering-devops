f453b2ccfa9d52c8bfceef82b82437d89def9937
Aughostinos <augustinos.nabil@gmail.com>
2024-06-19 04:15:32 +0400

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     If an invalid subreddit is given, returns 0.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": 'my_reddit_app/0.1 (by /u/augustinos_abusaif)'}
+    headers = {"User-Agent": 'my_reddit_app/0.1 (by /u/AugustinosAbusaif)'}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
