6f55e0fdff24036b64a86995dccec71d163cf073
Aughostinos <augustinos.nabil@gmail.com>
2024-06-19 05:06:24 +0400

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,18 +3,39 @@
 import requests
 
 
+def get_reddit_token(client_id, client_secret, username, password):
+    auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
+    data = {'grant_type': 'password', 'username': username, 'password': password}
+    headers = {'User-Agent': 'myAPIbot/0.1'}
+
+    response = requests.post('https://www.reddit.com/api/v1/access_token', auth=auth, data=data, headers=headers)
+    response.raise_for_status()
+    token = response.json().get('access_token')
+    return token
+
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers for a
-    given subreddit. If an invalid subreddit is given, returns 0.
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    If an invalid subreddit is given, returns 0.
     """
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+    client_id = 'I89Gxobf1Iskg-ZjHb_9cg'
+    client_secret = '68NzNnNJqk3g0dVDprHo6eORqQepeQ'
+    username = 'AugustinosAbusaif'
+    password = 'Alx@123#!'
+    token = get_reddit_token(client_id, client_secret, username, password)
+
+    if not token:
+        print("Failed to get access token")
+        return 0
+
+    url = f'https://oauth.reddit.com/r/{subreddit}/about'
+    headers = {'Authorization': f'bearer {token}', 'User-Agent': 'myAPIbot/0.1'}
 
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+        response = requests.get(url, headers=headers)
         print(f"Response status code: {response.status_code}")
         print(f"Response headers: {response.headers}")
+        print(f"Response content: {response.content.decode('utf-8')}")
 
         if response.status_code == 200:
             data = response.json()
