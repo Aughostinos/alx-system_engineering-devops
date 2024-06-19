326d59c660cbdd825b04dadd03cdb3fd532843a5
Aughostinos <augustinos.nabil@gmail.com>
2024-06-19 04:44:55 +0400

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    If an invalid subreddit is given, returns 0.
+    Queries the Reddit API and returns the number of subscribers for a
+    given subreddit. If an invalid subreddit is given, returns 0.
     """
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-agent': 'myAPIbot/0.1'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
