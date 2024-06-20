f38e22b0f5f0448e886de06eed1b3308e4830985
Aughostinos <augustinos.nabil@gmail.com>
2024-06-20 06:18:21 +0400

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,49 +2,16 @@
 """api advanced project"""
 import requests
 
-
-def get_reddit_token(client_id, client_secret, username, password):
-    """authentication"""
-    auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
-    data = {
-        'grant_type': 'password',
-        'username': username,
-        'password': password
-    }
-    headers = {'User-Agent': 'Augustbot'}
-
-    response = requests.post(
-        'https://www.reddit.com/api/v1/access_token',
-        auth=auth, data=data, headers=headers
-    )
-    response.raise_for_status()
-    token = response.json().get('access_token')
-    return token
-
-
 def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of subscribers for a given
     subreddit. If an invalid subreddit is given, returns 0.
     """
-    client_id = 'I89Gxobf1Iskg-ZjHb_9cg'
-    client_secret = '68NzNnNJqk3g0dVDprHo6eORqQepeQ'
-    username = 'AugustinosAbusaif'
-    password = 'Alx@123#!'
-    token = get_reddit_token(client_id, client_secret, username, password)
-
-    if not token:
-        print("Failed to get access token")
-        return 0
-
-    url = f'https://oauth.reddit.com/r/{subreddit}/about'
-    headers = {
-        'Authorization': f'bearer {token}',
-        'User-Agent': 'myAPIbot/0.1'
-    }
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'Augustbot'}
 
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers, allow_redirects=False)
 
         if response.status_code == 200:
             data = response.json()
