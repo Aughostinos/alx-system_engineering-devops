70e6404d2f134e24fd901b0942946578241c247f
Aughostinos <augustinos.nabil@gmail.com>
2024-06-29 02:25:31 +0400

    fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,11 +2,13 @@
 #change the soft and hard limits for holberton user
 
 exec { 'set_holberton_hard_limit':
-  command => '/bin/sed -i "s/holberton hard nofile [0-9]*/holberton hard nofile 4096/" /etc/security/limits.conf || echo "holberton hard nofile 4096" >> /etc/security/limits.conf',
-  path    => 'usr/local/bin/:/bin/'
+  command => '/bin/sed -i "s/holberton hard nofile [0-9]*/holberton hard nofile 4096/" /etc/security/limits.conf || \
+             echo "holberton hard nofile 4096" >> /etc/security/limits.conf',
+  path    => 'usr/local/bin/:/bin/',
 }
 
 exec { 'set_holberton_soft_limit':
-  command => '/bin/sed -i "s/holberton soft nofile [0-9]*/holberton soft nofile 4096/" /etc/security/limits.conf || echo "holberton soft nofile 4096" >> /etc/security/limits.conf',
-  path    => 'usr/local/bin/:/bin/'
+  command => '/bin/sed -i "s/holberton soft nofile [0-9]*/holberton soft nofile 4096/" /etc/security/limits.conf || \
+             echo "holberton soft nofile 4096" >> /etc/security/limits.conf',
+  path    => 'usr/local/bin/:/bin/',
 }
